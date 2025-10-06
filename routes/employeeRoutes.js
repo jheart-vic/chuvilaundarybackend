a7950f4be738d49c2c9062b87dd4d3ccfe667acf
jheart-vic <adebowalevictorjheart@gmail.com>
2025-10-06 09:23:06 +0100

    update import

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { employeeLogin } from '../controllers/employeeController';
+import { employeeLogin } from '../controllers/employeeController.js';
+
 
 const router = express.Router();
 
