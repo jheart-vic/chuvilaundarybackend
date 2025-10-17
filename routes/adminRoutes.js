a16246538ac9ec50b87b0037566d678d4c177f3a
jheart-vic <adebowalevictorjheart@gmail.com>
2025-10-17 22:12:58 +0100

    by id

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -59,7 +59,7 @@ router.post("/plans", requireAuth, requireAdmin, createPlan);
 router.put("/plans/:code", requireAuth, requireAdmin, updatePlan);
 router.delete("/plans/:code", requireAuth, requireAdmin, deactivatePlan);
 router.patch('/plans/:code/activate',requireAuth, requireAdmin, activatePlan);
-router.get('/plan', requireAuth, requireAdmin,  getSinglePlan)
+router.get('/plan/:id', requireAuth, requireAdmin,  getSinglePlan)
 router.get("/plans/active", requireAuth, requireAdmin, listActivePlans);
 
 //issues routes
